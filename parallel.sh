3149bfb7bd475c91346c1a73aff3c70851fdcea2
Marissa Roseman <marissa.roseman@gmail.com>
2025-01-13 18:49:14 -0500

    reorganizing files

--- parallel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-## Called by 6_fgenesh2gff.sh
-
-cat /fs/scratch/PAS1533/marissa/MHC/6_fgenesh2gff/startcoords.list | parallel 'name=$(echo {} | cut -d, -f1); start=$(echo {} | cut -d, -f2); sh /fs/scratch/PAS1533/marissa/MHC/scripts/awk.sh ${name} ${start}'
